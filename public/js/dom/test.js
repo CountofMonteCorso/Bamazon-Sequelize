5e8f18d38d43a08dda3280abd89f91b51910f57c
Miotti <210061319@LOGON.DS.GE.COM>
2019-02-09 10:42:43 -0500

    Unit Testing Updates

--- public/js/dom/test.js
+++ public/js/dom/test.js
@@ -1,28 +1,36 @@
 describe('setNameQuanityCostToDom', function() {
+
     it('should populate name, quantity and cost divs', function() {
-        // do some dom triggering or whatever
         setNameQuanityCostToDom('radio', 3, 75.50)
         expect($('#product_name').text()).to.equal('Prodcut Name: radio');
     })
 
     it('Expect should not be empty populate Quantity div', function() {
-        // do some dom triggering or whatever
         setNameQuanityCostToDom('radio', 1, 28250)
         expect($('#product_name').text()).to.be.a('string');
     })
 
     it('Order Quantity should equal 50', function() {
-        // do some dom triggering or whatever
         setNameQuanityCostToDom('TV', 50, 825.00)
         expect($('#order_quantity').text()).to.equal('Order Quantity: 50');
     })
   
     it('Order Total Cost should not be empty', function() {
-        // do some dom triggering or whatever
-        setNameQuanityCostToDom('radio', 1, 28250)
+        setNameQuanityCostToDom('radio', 1, 75)
         expect($('#order_cost').text()).to.not.be.empty;
     })
 
- 
+    it('Order Total Cost should not include a negative cost value', function() {
+        setNameQuanityCostToDom('radio', 1, -75)
+        expect($('#order_cost').text()).to.not.include('-');
+    })
+
+
+    it('Order Quantity should not equal 0', function() {
+        setNameQuanityCostToDom('TV', 0, 825.00)
+        expect($('#order_quantity').text()).to.not.equal('Order Quantity: 0');
+    })
+  
+
 });
 
